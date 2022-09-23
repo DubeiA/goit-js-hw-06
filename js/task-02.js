4533061a8e45d46a7c366dd0de1e07f0762a3885
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-09-23 19:11:12 +0300

    Task #2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,34 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
+  'допомагав Google:)'
 ];
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const listIngrediens = document.querySelector('ul')
+
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  
+  const itemIngredients = document.createElement('li');
+  itemIngredients.classList.add('item')
+  itemIngredients.innerHTML = ingredients[i];
+  listIngrediens.appendChild(itemIngredients);
+}
+
+console.log(listIngrediens);
+
+
+// const firstItemIngredients = document.createElement('li');
+// firstItemIngredients.classList.add('item')
+// console.log(firstItemIngredients);
+
+
+// const uniqueGenres = ingredients.map((ingredients) => ingredients);
+// console.log(uniqueGenres);
