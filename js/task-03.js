071d9fd056022480c07aef2db9ca2c756129a2c3
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-09-26 09:11:22 +0300

    done task 3 + add styles on pic

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,24 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+console.log(images);
+
+
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+const markup = images.map((image) => `<li class="gallery-item"><img class="pic" src=${image.url} alt=${image.alt}></li>`);
+
+console.log(markup);
+
+galleryEl.insertAdjacentHTML('beforeend', markup)
+
+
+// for (let i = 0; i < images.length; i += 1) {
+  
+//   const itemIngredients = document.createElement('li');
+//   itemIngredients.classList.add('item')
+//   itemIngredients.innerHTML = images[i];
+//   galleryEl.appendChild(itemIngredients);
+// }
