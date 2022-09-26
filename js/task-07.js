ff0472713bdb69026ac2b6a66fd89d1c1fbf3864
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-09-26 12:53:05 +0300

    done task 7

---
+++ js/task-07.js
@@ -0,0 +1,18 @@
+
+
+
+const fontSizeControl = document.querySelector('#font-size-control');
+const textSize = document.querySelector('#text');
+
+console.log(fontSizeControl.value);
+console.log(textSize.textContent);
+// fontSize.addEventListener("input");
+// textSize.addEventListener('change');
+
+
+fontSizeControl.addEventListener("input", () => {
+
+    textSize.style.fontSize = fontSizeControl.value + "px";
+    
+});
+
